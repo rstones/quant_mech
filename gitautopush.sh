a8adf08308df362671f7179a2b9c09e4902dbdb8
Richard Stones <r.stones@ucl.ac.uk>
2014-11-07 11:13:40 +0000

    Auto-commit at Fri Nov  7 11:13:40 GMT 2014

--- gitautopush.sh
+++ gitautopush.sh
@@ -1,6 +1,14 @@
-clear
-#git checkout -b development
-cd /home/rstones/git/quant_mech
+#!/bin/bash
+
+# Script to commit all changes in development branch of git project
+# and push to remote repository. Development branch must exist and
+# credentials for pushing to remote repo must be set up for this to 
+# work. This script is run by a cron job and takes a single parameter
+# which is the top level folder of the git project.
+# Logs for the cron job are in the file /var/spool/mail/rstones
+
+cd /home/rstones/git/$1
+git checkout development
 git add ./
 now=$(date)
 git commit -m "Auto-commit at $now"
