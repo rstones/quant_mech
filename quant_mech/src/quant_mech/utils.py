d8b15e6f6b4f713dff4dd49095af0f2a0f3e05b8
Richard Stones <r.stones@ucl.ac.uk>
2015-08-03 22:00:03 +0100

    Auto-commit at Mon Aug  3 22:00:03 BST 2015

--- quant_mech/src/quant_mech/utils.py
+++ quant_mech/src/quant_mech/utils.py
@@ -155,6 +155,55 @@ def classical_stationary_state(liouvillian):
     stationary_state = stationary_state / np.trace(stationary_state)
     return np.diagonal(stationary_state)
 
+def nullspace(A, atol=1e-14, rtol=0):
+    """Compute an approximate basis for the nullspace of A.
+    
+    The algorithm used by this function is based on the singular value
+    decomposition of `A`.
+    
+    Parameters
+    ----------
+    A : ndarray
+        A should be at most 2-D.  A 1-D array with length k will be treated
+        as a 2-D with shape (1, k)
+    atol : float
+        The absolute tolerance for a zero singular value.  Singular values
+        smaller than `atol` are considered to be zero.
+    rtol : float
+        The relative tolerance.  Singular values less than rtol*smax are
+        considered to be zero, where smax is the largest singular value.
+    
+    If both `atol` and `rtol` are positive, the combined tolerance is the
+    maximum of the two; that is::
+        tol = max(atol, rtol * smax)
+    Singular values smaller than `tol` are considered to be zero.
+    
+    Return value
+    ------------
+    ns : ndarray
+        If `A` is an array with shape (m, k), then `ns` will be an array
+        with shape (k, n), where n is the estimated dimension of the
+        nullspace of `A`.  The columns of `ns` are a basis for the
+        nullspace; each element in numpy.dot(A, ns) will be approximately
+        zero.
+    """
+    
+    A = np.atleast_2d(A)
+    u, s, vh = la.svd(A)
+    tol = max(atol, rtol * s[0])
+    nnz = (s >= tol).sum()
+    ns = vh[nnz:].conj().T
+    return ns
+
+'''
+Find stationary state of Liouvillian using a singular value decomposition which is much faster than diagonalisation
+and finding the eigenvector associated with the zero eigenvalue. An array indicating the position of populations of 
+the density vector allows correct normalization (1 for position elements, 0 for coherence elements).
+'''
+def stationary_state_svd(liouvillian, density_vector_populations):
+    ss = nullspace(liouvillian); ss = ss.flatten()
+    return ss / np.sum(ss * density_vector_populations) # normalise
+
 # differentiates a function defined for points on an array
 # f is dependent variable
 # x is independent variable
