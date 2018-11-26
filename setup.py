4dc8f59d66e131cca0cc896638f548d9fcae66e4
Rising Up! <richardstones10@gmail.com>
2018-11-26 11:15:04 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="quant_mech",
+    version="0.1",
+    author_email="richardstones10@gmail.com",
+    description="",
+    packages=["quant_mech.src.quant_mech"]
+)
